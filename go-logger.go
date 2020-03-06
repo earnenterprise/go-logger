59d18d4cf610ebaa06a3996e55b9a8447bd055fa
David Larsson <dlarsson.se@gmail.com>
2020-03-06 23:21:32 +0100

    Updated colors.

--- go-logger.go
+++ go-logger.go
@@ -80,19 +80,19 @@ type jsonLOG struct {
 
 const (
 	// InfoColor ...
-	InfoColor = "\033[0;36m%s\033[0m"
+	InfoColor = "\033[0;32m%s\033[0m"
 
 	// NoticeColor ...
-	NoticeColor = "\033[1;36m%s\033[0m"
+	NoticeColor = "\033[0;36m%s\033[0m"
 
 	// WarningColor ...
-	WarningColor = "\033[1;33m%s\033[0m"
+	WarningColor = "\033[0;33m%s\033[0m"
 
 	// ErrorColor ...
-	ErrorColor = "\033[1;31m%s\033[0m"
+	ErrorColor = "\033[0;31m%s\033[0m"
 
 	// DebugColor ...
-	DebugColor = "\033[1;34m%s\033[0m"
+	DebugColor = "\033[0;34m%s\033[0m"
 )
 
 // NewCustom ...
